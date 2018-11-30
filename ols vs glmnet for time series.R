5a9d2afd7b3175813217174673ff205e35a22762
khoffm4 <bym7444@gmail.com>
2018-11-29 22:33:39 -0500

    Swityched to 2017 Data

--- ols vs glmnet for time series.R
+++ ols vs glmnet for time series.R
@@ -1,5 +1,6 @@
 library(readr)
 demand_data <- read_csv("UNC/Classes 2018 Fall/COMP 755 -Machine Learning/Project/Demand_Data2.csv")
+demand_data <- read_csv("/Users/kentarohoffman/Documents/Github/Hoffman_Leinwand_COMP755_Project/Demand_Data2.csv")
 
 
 #Put June as the test set, all previous months as training set
@@ -11,18 +12,33 @@ train["Fold"] = ceiling(runif(dim(train)[1], min = 0, max=10))
 
 #Fit a model on 9 out of 10 folds 10 times
 for(i in 1:10){
+  print(i)
 assign(paste0("fit", i), lm(passenger_count~pickup_zip + factor(Hour)*factor(Day_of_week)+ factor(Hour)*factor(Month)+ factor(Day_of_week)*factor(Month)+ factor(Hour)*factor(Day_of_week)*factor(Month), data= subset(train, Fold !=i)))
-       
-       }
+  }
 
 
 #Based on the model fit without the left out fold, calculate the prediction of the true value of the holdout fold
 for(j in 1:dim(train)[1]){
+  print(j/dim(train)[1])
 train[j,"OLS_CV_Prediction"]=predict.lm(object = eval(parse(text=paste0("fit", train[j, "Fold"] ))), newdata = train[j, ])
 }
 
+#2nd order fit 
+for(i in 1:10){
+  print(i)
+  assign(paste0("fit", i), lm(passenger_count~(pickup_zip + factor(Hour)*factor(Day_of_week)+ factor(Hour)*factor(Month)+ factor(Day_of_week)*factor(Month)+ factor(Hour)*factor(Day_of_week)*factor(Month))^2, data= subset(train, Fold !=i)))
+}
 
-write.csv(train, file = "C:/Users/blein/Documents/UNC/Classes 2018 Fall/COMP 755 -Machine Learning/Project/Demand_Data3_Train.csv")
+
+#Based on the model fit without the left out fold, calculate the prediction of the true value of the holdout fold
+for(j in 1:dim(train)[1]){
+  print(j/dim(train)[1])
+  train[j,"OLS2_CV_Prediction"]=predict.lm(object = eval(parse(text=paste0("fit", train[j, "Fold"] ))), newdata = train[j, ])
+}
+
+
+write.csv(train, file = "C:/Users/blein/Documents/UNC/Classes 2018 Fall/COMP 755 -Machine Learning/Project/Demand_Data4_Train.csv")
+write.csv(train, file = "/Users/kentarohoffman/Documents/Github/Hoffman_Leinwand_COMP755_Project/Demand_Data4.csv")
 
 ###########################################################
 
