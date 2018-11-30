ae71a5a6fee03cd3d0206426d63a3f02f865fd78
khoffm4 <bym7444@gmail.com>
2018-11-29 23:16:09 -0500

    Working on LM

--- ols vs glmnet for time series.R
+++ ols vs glmnet for time series.R
@@ -1,11 +1,13 @@
 library(readr)
 demand_data <- read_csv("UNC/Classes 2018 Fall/COMP 755 -Machine Learning/Project/Demand_Data2.csv")
-demand_data <- read_csv("/Users/kentarohoffman/Documents/Github/Hoffman_Leinwand_COMP755_Project/Demand_Data2.csv")
+demand_data <- read_csv("/Users/kentarohoffman/Documents/Github/Hoffman_Leinwand_COMP755_Project/Demand_Data2_2017.csv")
+
+demand_data <- read_csv("/Users/kentarohoffman/Documents/Github/Hoffman_Leinwand_COMP755_Project/Demand_Data2_2017.csv")
 
 
 #Put June as the test set, all previous months as training set
-train=subset(demand_data, Month<6)
-test = subset(demand_data, Month==6)
+train=subset(demand_data, Month<11)
+test = subset(demand_data, Month>10)
 
 #Cut the training set into 10 folds
 train["Fold"] = ceiling(runif(dim(train)[1], min = 0, max=10))
@@ -13,8 +15,9 @@ train["Fold"] = ceiling(runif(dim(train)[1], min = 0, max=10))
 #Fit a model on 9 out of 10 folds 10 times
 for(i in 1:10){
   print(i)
-assign(paste0("fit", i), lm(passenger_count~pickup_zip + factor(Hour)*factor(Day_of_week)+ factor(Hour)*factor(Month)+ factor(Day_of_week)*factor(Month)+ factor(Hour)*factor(Day_of_week)*factor(Month), data= subset(train, Fold !=i)))
-  }
+assign(paste0("fit", i), lm(passenger_count~PULocationID + factor(Hour)*factor(Day_of_week)+ factor(Hour)*factor(Month)+ factor(Day_of_week)*factor(Month)+ factor(Hour)*factor(Day_of_week)*factor(Month), data= subset(train, Fold !=i)))
+print(Sys.time())  
+}
 
 
 #Based on the model fit without the left out fold, calculate the prediction of the true value of the holdout fold
